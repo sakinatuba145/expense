f66d4b0d717cff0ca6e3f7d853415fdfec239a6d
sakinatuba145 <sakinatuba145@gmail.com>
2026-01-28 21:18:40 +0430

    Create TextInput.jsx

---
+++ second-project/src/components/TextInput.jsx
@@ -0,0 +1,15 @@
+export default function TextInput
+({label, value, onChange, placeholder, type="text" }){
+
+  return (
+    <div className="field">
+        <label className="label">{label}</label>
+         <input 
+         type={type}
+         value={value}
+         placeholder={placeholder}
+         onChange={(e)=> onChange(e.target.value)} 
+         className="input" />
+         </div>
+  )  
+}
